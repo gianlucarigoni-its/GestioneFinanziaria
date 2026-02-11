de82a9f61a3bb2b7aa3658e90022a9751fad3fc5
gianlucarigoni-its <gianlucarigoni@allievi.itsdigitalacademy.com>
2026-02-11 16:17:21 +0100

    prova creazione user in supabase

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import { supabase } from "./config/supabase";
 
 const app = express();
 app.use(bodyparser.json());
 
+async function createGigiluca(){
+    const user = await supabase.auth.signUp({
+        "email" : "gigilucagglc@gmal.com",
+        "password" : "123456789"
+    });
 
+    console.log(user.data.user);
+}
+
+createGigiluca();
 export default app;
 
